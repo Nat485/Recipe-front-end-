c4595e6ac13963f3bfb1543af4b672ca53641ecd
Nat <nat485@gmail.com>
2024-02-26 13:51:04 -0500

    updated errors

--- src/components/routes/RouteComponent.jsx
+++ src/components/routes/RouteComponent.jsx
@@ -6,6 +6,12 @@ import Show from "../../pages/Show.jsx"
 import Error from "../../pages/Error.jsx"
 import Recipes from "../../pages/Recipes.jsx"
 
+
+
+//import { useNavigate, Navigate }  from "react-router-dom"
+// Use navigate is used after something happens, doesn't start unless something happens.
+// Navigate is used as a component that can be rendered nothing needs to happen in order for it to trigger it, it will just start
+
 // Functional component named RouteComponent
 export default function RouteComponent() {
     return (
@@ -19,6 +25,15 @@ export default function RouteComponent() {
                 <Route path="/recipes/:recipeID" element = { <Show />} />   //to obtain 1 id 
             
                 <Route path= "/recipes/new" element = { <New />} />
+
+                <Route path = "/recipes/:recipeID/edit" element = { <Edit />} />
+
+                <Route path = "/error" element = { <Error />} />
+
+                //Route path = "*" element = {<p>Wildcard</p>} 
+
+
+
 
 
 
