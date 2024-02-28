1da6894a8e65f7c906b93d7690d937561394e507
Nat <nat485@gmail.com>
2024-02-28 11:17:12 -0500

    cleaned up errors

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import RouteComponent from './components/routes/RouteComponent.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='App'> 
+   <RouteComponent/>
+
+   </div>
+  
+  )
+  
+ }
+export default App
