fedb29b012730894a7604993b6ce0545ed9bb79c
Nat <nat485@gmail.com>
2024-03-07 13:07:48 -0500

    added forms

--- src/forms/Form.jsx
+++ src/forms/Form.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import './Form.css'
 
-export default function Form() {
+const API = import.meta.env.VITE_APP_API_URL
 
-
-const [form, setForm] = useState({
+function Form() {
+    const [form, setForm] = useState({
     recipe_name:"",
     creation_date:"",
     vegan: false,
@@ -14,10 +16,22 @@ const [form, setForm] = useState({
     
 const nav = useNavigate();
 
+const { id } = useParams()
+
+useEffect(() => {
+    if(id) {
+        axios.get('${API}/bourbons/${id}')
+            .then(res => setForm(res.data))
+            .catch(err => console.log(err))
+
+    }
+}
+)
+
 function handleTextInput(event) {
     const id = event.target.id;
-    const value = event.target.value;
-
+    const value = event.target.value
+}
     //setForm({form, [id] : value})
 
     setForm((currentState) => {
@@ -28,3 +42,42 @@ function handleTextInput(event) {
         };
     });
 }}
+
+function newRecipeAdded(event) {
+    event.preventDefault()
+
+    axios.post(`${API}/recipes`, form)
+        .then(res => Navigate(`/recipes/${res.id}`))
+        .catch(error => console.log(error))
+}
+
+function submitEditedForm(event) {
+    event.preventDefault()
+
+    axios.put(`${API}recipe/${id}`, form)
+        .then(res => navigate(`/bourbons/${res.id}`))
+        .catch(error => console.log(error))
+}
+
+return (
+    <form className="form">
+
+        {     }   //recipe_name
+        <label htmlFor=' recipe_name'>
+            <span>Recipe Name:</span>
+            <input
+            id='recipe_name'
+            type='text'
+            value={form.recipe_name}
+            required
+            onChange={(e) => { handleTextInput(e)}
+            />
+            </label>
+            <label htmlFor=" "
+            </input>
+    </form>
+)
+
+
+
+export function Form
