aec7940308654579aa6289f72828612b71174b39
Nat <nat485@gmail.com>
2024-03-18 10:10:04 -0400

    fixed errors in the form jsx file

--- src/forms/Form.jsx
+++ src/forms/Form.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import './Form.css';
 
@@ -14,53 +13,54 @@ function Form() {
         recipe_type: ""
     });
 
+    const [selectedValue, setSelectedValue] = useState(""); // this will be used for the value that the user will choose from the dropdown menu
+
     const { id } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (id) {
-            axios.get(`${API}/recipes/${id}`)
+            axios.get(`${API}/gfrecipes/${id}`)
                 .then(res => setForm(res.data))
                 .catch(err => console.log(err));
         }
     }, [id, API]);
 
     function handleTextInput(event) {
-        const id = event.target.id;
-        let value = event.target.value;
+        const { id, value, type, checked } = event.target;
 
-        //for the boolean like vegan
-        if (event.target.type === 'checkbox') {
-        value = event.target.checked; //Handle boolean value for a checkbox
-        }
-        console.log(value)
+        // Handle boolean value for a checkbox
+        const newValue = type === 'checkbox' ? checked : value;
 
-        setForm((currentState) => ({
-            ...currentState,
-            [id]: value,
+        setForm(prevState => ({
+            ...prevState,
+            [id]: newValue
         }));
     }
 
     function newRecipe(event) {
         event.preventDefault();
 
-        axios.post(`${API}/recipes`, form)
-            .then(res => navigate(`/recipes/${res.data.id}`))
+        axios.post(`${API}/gfrecipes`, form)
+            .then(res => navigate(`/gfrecipes/${res.data.id}`))
             .catch(error => console.log(error));
     }
 
     function submitEditedForm(event) {
         event.preventDefault();
 
-        axios.put(`${API}/recipe/${id}`, form)
-            .then(res => navigate(`/recipe/${res.data.id}`))
+        axios.put(`${API}/gfrecipes/${id}`, form)
+            .then(res => navigate(`/gfrecipes/${res.data.id}`))
             .catch(error => console.log(error));
+    }
+
+    function handleSelectChange(event) {
+        setSelectedValue(event.target.value)
     }
 
     return (
         <form className="form">
-              
-              {/* recipe_name */}
+            {/* recipe_name */}
             <label htmlFor='recipe_name'>
                 <span>Recipe Name:</span>
                 <input
@@ -72,47 +72,48 @@ function Form() {
                 />
             </label>
           
-           {/* creation_date */}
-           <label htmlFor='creation_date'>
+            {/* creation_date */}
+            <label htmlFor='creation_date'>
                 <span>Creation Date:</span>
                 <input
                     id='creation_date'
-                    type='int'
+                    type="date"
                     value={form.creation_date}
                     required
                     onChange={(e) => handleTextInput(e)}
                 />
             </label>
 
-             {/* vegan */}
-           <label htmlFor='vegan'>
+            {/* vegan */}
+            <label htmlFor='vegan'>
                 <span>Vegan:</span>
                 <input
                     id='vegan'
                     type='checkbox'
-                    value={form.vegan}
+                    checked={form.vegan}
                     required
                     onChange={(e) => handleTextInput(e)}
                 />
             </label>
 
-            {/* recipe type */}
-           <label htmlFor='recipe_type'>
+            <label htmlFor='recipe_type'>
                 <span>Recipe Type:</span>
-                <input
+                <select
                     id='recipe_type'
-                    type='text'
-                    value={form.recipe_type}
+                    value={selectedValue}
                     required
-                    onChange={(e) => handleTextInput(e)}
-                />
+                    onChange={handleSelectChange}
+                >
+                    <option value=''>Select a type</option>
+                    <option value='Apps'>Apps</option>
+                    <option value='Mains'>Mains</option>
+                    <option value='Sweets'>Sweets</option>
+                </select>
             </label>
-       
-    
-    <button id="showFormButton">Show Form</button>
-    </form>
-     );
- }
- 
+
+            <button id="addRecipeButton" onClick={id ? submitEditedForm : newRecipe}>Add recipe</button>
+        </form>
+    );
+}
 
 export default Form;
